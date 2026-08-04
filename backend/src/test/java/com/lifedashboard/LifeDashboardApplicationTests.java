3b316c9cf94e9518fed767482074a6af907e9ec5
Denis <domino387@gmail.com>
2026-08-04 19:06:54 +0300

    Set up Spring Boot backend and PostgreSQL schema

---
+++ backend/src/test/java/com/lifedashboard/LifeDashboardApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lifedashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
